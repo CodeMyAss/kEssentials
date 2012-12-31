56a352b4acf210519e843b6fd10f1eedb5ffc7aa
KeybordPiano459 <jbcook123@gmail.com>
2012-12-31 16:47:41 -0500

    Reformatted kChat code

--- kChat/src/me/KeybordPiano459/kChat/ChatColors.java
+++ kChat/src/me/KeybordPiano459/kChat/ChatColors.java
@@ -1,20 +1,26 @@
 package me.KeybordPiano459.kChat;
 
-import me.KeybordPiano459.kEssentials.util.ChatManager;
-
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class ChatColors implements Listener {
-	@EventHandler
-	public void onChat(AsyncPlayerChatEvent event) {
-		Player player = event.getPlayer();
-		String msg = event.getMessage();
-		if (kChat.colorallowed)
-			event.setMessage(ChatManager.colorCompatible(msg)); // Add color symbols to chat messages
-		if (player.isOp() && kChat.opcolor != "none")
-			event.setFormat("<" + ChatManager.colorCompatible(kChat.opcolor + player.getName()) + "> " + event.getMessage());
-	}
+    static kChat plugin;
+    public ChatColors(kChat plugin) {
+        ChatColors.plugin = plugin;
+    }
+    
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent event) {
+        Player player = event.getPlayer();
+        String msg = event.getMessage();
+        if (plugin.colorallowed) {
+            event.setMessage(ChatColor.translateAlternateColorCodes('&', msg));
+        } // Add color symbols to chat messages
+        if (player.isOp() && !plugin.opcolor.equals("none")) {
+            event.setFormat(ChatColor.translateAlternateColorCodes('&', "<" + player.getName() + "> " + msg));
+        }
+    }
 }
--- kChat/src/me/KeybordPiano459/kChat/ChatRadius.java
+++ kChat/src/me/KeybordPiano459/kChat/ChatRadius.java
@@ -1,7 +1,6 @@
 package me.KeybordPiano459.kChat;
 
 import java.util.List;
-
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -9,18 +8,23 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class ChatRadius implements Listener {
-	@EventHandler
-	public void onChat(AsyncPlayerChatEvent event) {
-		Player player = event.getPlayer();
-		int nearbyn = kChat.chatradius;
-		if (nearbyn != 0) {
-			List<Entity> nearby = player.getNearbyEntities(nearbyn, nearbyn, nearbyn);
-			for (Entity e : nearby) {
-				if (e instanceof Player) {
-					Player p = (Player) e;
-					p.sendMessage("<" + player.getName() + "> " + event.getMessage());
-				}
-			}
-		}
-	}
+    static kChat plugin;
+    public ChatRadius(kChat plugin) {
+        ChatRadius.plugin = plugin;
+    }
+    
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent event) {
+        Player player = event.getPlayer();
+        int nearbyn = plugin.chatradius;
+        if (nearbyn != 0) {
+            List<Entity> nearby = player.getNearbyEntities(nearbyn, nearbyn, nearbyn);
+            for (Entity e : nearby) {
+                if (e instanceof Player) {
+                    Player p = (Player) e;
+                    p.sendMessage("<" + player.getName() + "> " + event.getMessage());
+                }
+            }
+        }
+    }
 }
