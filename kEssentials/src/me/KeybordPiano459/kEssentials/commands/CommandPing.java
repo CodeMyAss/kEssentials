599be25d34ba49a25201d21699be3e7dde7418ad
Mark Hendriks <datfireblast709@gmail.com>
2013-01-03 00:45:37 +0100

    Multi version ping
    Ping should be supported by multiple versions

--- kEssentials/src/me/KeybordPiano459/kEssentials/commands/CommandPing.java
+++ kEssentials/src/me/KeybordPiano459/kEssentials/commands/CommandPing.java
@@ -1,15 +1,45 @@
 package me.KeybordPiano459.kEssentials.commands;
 
+import java.lang.reflect.Field;
 import java.util.logging.Level;
 import me.KeybordPiano459.kEssentials.kEssentials;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class CommandPing extends kCommand implements CommandExecutor {
+    
+    String packageV = "";
+    
     public CommandPing(kEssentials plugin) {
         super(plugin);
+        
+        String bv = Bukkit.getVersion();
+        int index = bv.indexOf("(MC: ")+5;
+        int end = bv.indexOf(")", index);
+        String version = bv.substring(index, end);
+        plugin.getLogger().info(new StringBuilder("Version found: ").append(version).toString()); 
+        try
+        {
+            String[] vs = version.split(".");
+            if(Integer.parseInt(vs[0]) >= 1)
+            {
+                if(Integer.parseInt(vs[1]) >= 4)
+                {
+                    if(Integer.parseInt(vs[2]) >= 5)
+                    {
+                        packageV = new StringBuilder(".v").append(version.replace(".", "_")).toString();
+                    }
+                }
+            }
+        }
+        catch(Exception ex)
+        {
+            //plugin.getLogger().info("Version lower than 1.4.5 found");
+        }
+        
     }
     
     @Override
@@ -19,9 +49,21 @@ public class CommandPing extends kCommand implements CommandExecutor {
                 Player player = (Player) sender;
                 if (args.length == 0) {
                     if (player.hasPermission("kessentials.ping")) {
-                        CraftPlayer cp = (CraftPlayer) player;
-                        int ping = cp.getHandle().ping;
-                        player.sendMessage(GREEN + "Pong" + ping + "MS");
+                        try
+                        {
+                            Object nmsPlayer = Class.forName("org.bukkit.craftbukkit"+packageV+".entity.CraftPlayer")
+                                                .getMethod("getHandle", new Class[0])
+                                                .invoke(player, new Object[0]);
+                            Field con = Class.forName("net.minecraft.server"+packageV+".EntityPlayer").getDeclaredField("ping");
+                            con.setAccessible(true);
+                            int ping = (int)con.get(nmsPlayer);
+                            player.sendMessage(GREEN + "Pong " + ping + " MS");
+                        }
+                        catch(Exception ex)
+                        {
+                            player.sendMessage(RED + "Failed to collect ping data");
+                        }
+                        
                     } else {
                         noPermissionsMessage(player);
                     }
